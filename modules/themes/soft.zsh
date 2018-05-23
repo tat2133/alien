fe4cfc7eb69afa6fb55f5708faa888cdc989722c
Taylor Thompson <taylor.thompson@gartner.com>
2018-05-23 15:16:33 -0400

    changed to grey colors i liked better

--- modules/themes/soft.zsh
+++ modules/themes/soft.zsh
@@ -12,12 +12,12 @@ alien_theme(){
   [[ -z $color7 ]]  && color7=238          # vcs bg
   [[ -z $color8 ]]  && color8=${color1}    # prompt fg
   [[ -z $color9 ]]  && color9=${color1}    # vcs fg
-  [[ -z $color10 ]] && color10=${color7}   # lr bg
-  [[ -z $color11 ]] && color11=${color9}   # lr fg
-  [[ -z $color12 ]] && color12=${color7}   # dirty copy bg
-  [[ -z $color13 ]] && color13=${color9}   # dirty copy fg
-  [[ -z $color14 ]] && color14=${color7}   # venv color
-
-  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=${color0}
-  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=${color9}
+  [[ -z $color10 ]] && color10=244   # lr bg
+  [[ -z $color11 ]] && color11=255   # lr fg
+  [[ -z $color12 ]] && color12=253   # dirty copy bg
+  [[ -z $color13 ]] && color13=016   # dirty copy fg
+  [[ -z $color14 ]] && color14=245   # venv color
+  
+  [[ -z "${PLIB_GIT_TRACKED_COLOR}" ]]   && PLIB_GIT_TRACKED_COLOR=27
+  [[ -z "${PLIB_GIT_UNTRACKED_COLOR}" ]] && PLIB_GIT_UNTRACKED_COLOR=236
 }
